f14387398bff0f3a065ac3d256072c4e7603a28f
Akash kumar sahu <sahuakashkumar915@gmail.com>
2025-07-15 11:00:37 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app= express();
 const port = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:5173"}))
+app.use(cors({origin:"https://mssahu-inventory.onrender.com/"}))
 app.use("/admin",router)
 ConnectDb();
 app.get("/",(req,res)=>{
